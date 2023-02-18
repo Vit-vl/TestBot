da6b7546b706273bf3eaa7a60d5559e033490f36
Vit-vl <vit.pravo@gmail.com>
2023-02-19 01:00:27 +1000

    small changes

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-import requests
+import requests  # type: ignore
 import time
-import settings
+from settings import token
 
 API_URL: str = 'https://api.telegram.org/bot'
-TEXT: str = 'Ура! Работает!'
+TEXT: str = 'Здесь должна была быть картинка с котиком :('
+API_FOX: str = 'https://randomfox.ca/floof/'
 MAX_COUNTER: int = 30
 
 offset: int = -2
 counter: int = 0
 chat_id: int
+cat_response: requests.Response
+fox_link: str
 
 while counter < MAX_COUNTER:
-
-    print('attempt =', counter)  # Чтобы видеть в консоли, что код живет
-
-    updates = requests.get(f'{API_URL}{settings.BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    print('attempt =', counter)
+    updates = requests.get(f'{API_URL}{token}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{settings.BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
-
+            cat_response = requests.get(API_FOX)
+            if cat_response.status_code == 200:
+                fox_link = cat_response.json()['image']
+                requests.get(f'{API_URL}{token}/sendPhoto?chat_id={chat_id}&photo={fox_link}')
+            else:
+                requests.get(f'{API_URL}{token}/sendMessage?chat_id={chat_id}&text={TEXT}')
     time.sleep(1)
     counter += 1
